27a624a65642aff9a5efe5c9a8884899dbd3a72e
asnead <andrew.snead@atos.net>
2015-06-08 17:19:06 +0100

    finished basic scaffold sans flux architecture

--- spike/app/server.js
+++ spike/app/server.js
@@ -7,19 +7,27 @@ var bole = require("bole");
 var log = bole(__filename);
 bole.output({level: "debug", stream: process.stdout});
 process.on("uncaughtException", function uncaught(exception) {
-  log.error(exception, "uncaught exception. Process will exit");
+  log.error(exception, " uncaught exception. Process will exit");
   setImmediate(function exit() { 
     process.exit(66);
   }, 1000);
 });
 log.info("server process starting");
 
-mongoose.connect(config.dbURL);
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, 'connection error:'));
+var db;
+
+function DBConnect() {
+  mongoose.connect(config.dbURL);
+  db = mongoose.connection;
+}
+
+db.on("error", function(error) {
+  console.error(console, 'connection error:'));
 db.once("open", function(callback) {
   log.info("connected to: " + config.dbURL);
 });
+
+DBConnect();
 
 app.listen(config.express.port, config.express.ip, function (error) {
   if (error) {
