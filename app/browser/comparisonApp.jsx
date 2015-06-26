3a426e6d74788b16e4893dd686f0321e160ad2d1
asnead <andrew.snead@atos.net>
2015-06-26 11:43:48 +0100

    updating react with jsx syntax examples

--- app/browser/comparisonApp.jsx
+++ app/browser/comparisonApp.jsx
@@ -1,8 +1,11 @@
 var React = require("react");
-var ComparisonBox = React.createFactory(require("./comparisonContainer.jsx"));
+// Factory allows for the ComparisonContain() shorthand
+var ComparisonContainer = React.createFactory(require("./comparisonContainer.jsx"));
 
 React.render(
-   ComparisonBox(),
+   ComparisonContainer({boxes: [1, 2, 3, 4]}),
    document.getElementById("comparison-container")
 );
-     
+
+//Can use this JSX syntax if not using factory
+//<ComparisonContainer boxes={[1, 2, 3, 4]} />,
--- app/browser/comparisonContainer.jsx
+++ app/browser/comparisonContainer.jsx
@@ -1,9 +1,16 @@
 var React = require("react");
+var ComparisonBox = require("./comparisonBox.jsx");
 
 module.exports = ComparisonContainer = React.createClass({displayName: "ComparisonContainer",
   render: function() {
+    var comparisonBoxes = this.props.boxes.map(function (box) {
+      return (
+          <ComparisonBox />
+      );
+    });
     return (
         <section className="comparison-container">
+          {comparisonBoxes}
         </section>
     );
   }
