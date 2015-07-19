888558935876fdc89c97ae3ca58bb4d02628e314
asnead <andrew.snead@atos.net>
2015-07-19 22:28:02 +0100

    update react components

--- app/browser/comparisonDropdown.jsx
+++ app/browser/comparisonDropdown.jsx
@@ -6,7 +6,7 @@ module.exports = ComparisonDropdown = React.createClass({displayName: "Compariso
         <div>
           <p>{this.props.dropdownLabel}</p>
           <div className="DIV_8">
-            <select className="SELECT_9">
+            <select>
               <option value="94">BBC Worldwide</option>
               <option value="95">BNATIRELND</option>
               <option value="86">BNATWALE</option>
